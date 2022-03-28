de090bc81f2d0e86358e120217019be70f219666
innerpeacer <innerpeacer@gmail.com>
2022-03-28 10:08:31 +0800

    Init Commimt

---
+++ ARCameraDemo/ARCameraDemo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ARCameraDemo
+//
+//  Created by innerpeacer on 2022/3/28.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ ARCameraDemo/ARCameraDemo/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  ARCameraDemo
+//
+//  Created by innerpeacer on 2022/3/28.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
